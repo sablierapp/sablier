aba09e639bb54d383934be6ab4ea90e3778343d4
Stanislas Bruhiere <stanbill91@gmail.com>
2022-02-09 18:33:01 +0100

    Add tests for blocking strategy

--- pkg/strategy/dynamic_strategy_test.go
+++ pkg/strategy/dynamic_strategy_test.go
@@ -9,30 +9,9 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestDynamicStrategy_ServeHTTP(t *testing.T) {
-	testCases := []struct {
-		desc     string
-		status   string
-		expected int
-	}{
-		{
-			desc:     "service is starting",
-			status:   "starting",
-			expected: 202,
-		},
-		{
-			desc:     "service is started",
-			status:   "started",
-			expected: 200,
-		},
-		{
-			desc:     "ondemand service is in error",
-			status:   "error",
-			expected: 500,
-		},
-	}
+func TestSingleDynamicStrategy_ServeHTTP(t *testing.T) {
 
-	for _, test := range testCases {
+	for _, test := range SingleServiceTestCases {
 		test := test
 		t.Run(test.desc, func(t *testing.T) {
 			t.Parallel()
@@ -40,7 +19,7 @@ func TestDynamicStrategy_ServeHTTP(t *testing.T) {
 			next := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
 
 			mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				fmt.Fprint(w, test.status)
+				fmt.Fprint(w, test.onDemandServiceResponses[0].body)
 			}))
 
 			defer mockServer.Close()
@@ -57,93 +36,28 @@ func TestDynamicStrategy_ServeHTTP(t *testing.T) {
 
 			dynamicStrategy.ServeHTTP(recorder, req)
 
-			assert.Equal(t, test.expected, recorder.Code)
+			assert.Equal(t, test.expected.dynamic, recorder.Code)
 		})
 	}
 }
 
-func GenerateServicesStatuses(count int, status string) []string {
-	statuses := make([]string, count)
-	for i := 0; i < count; i++ {
-		statuses[i] = status
-	}
-	return statuses
-}
-
 func TestMultipleDynamicStrategy_ServeHTTP(t *testing.T) {
-	testCases := []struct {
-		desc     string
-		statuses []string
-		expected int
-	}{
-		{
-			desc:     "all services are starting",
-			statuses: GenerateServicesStatuses(5, "starting"),
-			expected: 202,
-		},
-		{
-			desc:     "one started others are starting",
-			statuses: append(GenerateServicesStatuses(1, "starting"), GenerateServicesStatuses(4, "started")...),
-			expected: 202,
-		},
-		{
-			desc:     "one starting others are started",
-			statuses: append(GenerateServicesStatuses(4, "starting"), GenerateServicesStatuses(1, "started")...),
-			expected: 202,
-		},
-		{
-			desc: "one errored others are starting",
-			statuses: append(
-				GenerateServicesStatuses(2, "starting"),
-				append(
-					GenerateServicesStatuses(1, "error"),
-					GenerateServicesStatuses(2, "starting")...,
-				)...,
-			),
-			expected: 500,
-		},
-		{
-			desc: "one errored others are started",
-			statuses: append(
-				GenerateServicesStatuses(1, "error"),
-				GenerateServicesStatuses(4, "started")...,
-			),
-			expected: 500,
-		},
-		{
-			desc: "one errored others are mix of starting / started",
-			statuses: append(
-				GenerateServicesStatuses(2, "started"),
-				append(
-					GenerateServicesStatuses(1, "error"),
-					GenerateServicesStatuses(2, "starting")...,
-				)...,
-			),
-			expected: 500,
-		},
-		{
-			desc:     "all are started",
-			statuses: GenerateServicesStatuses(5, "started"),
-			expected: 200,
-		},
-	}
-
-	for _, test := range testCases {
+	for _, test := range MultipleServicesTestCases {
 		test := test
 		t.Run(test.desc, func(t *testing.T) {
 			t.Parallel()
 
 			next := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
 
-			urls := make([]string, len(test.statuses))
-			for statusIndex, status := range test.statuses {
-				status := status
+			urls := make([]string, len(test.onDemandServiceResponses))
+			for responseIndex, response := range test.onDemandServiceResponses {
+				response := response
 				mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-					fmt.Fprint(w, status)
+					fmt.Fprint(w, response.body)
 				}))
 				defer mockServer.Close()
 
-				urls[statusIndex] = mockServer.URL
+				urls[responseIndex] = mockServer.URL
 			}
 			dynamicStrategy := &DynamicStrategy{
 				Name:     "whoami",
@@ -157,7 +71,7 @@ func TestMultipleDynamicStrategy_ServeHTTP(t *testing.T) {
 
 			dynamicStrategy.ServeHTTP(recorder, req)
 
-			assert.Equal(t, test.expected, recorder.Code)
+			assert.Equal(t, test.expected.dynamic, recorder.Code)
 		})
 	}
 }
