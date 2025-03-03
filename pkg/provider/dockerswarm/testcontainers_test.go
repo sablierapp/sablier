31c6011fcdf2efb44eefdb86fc52d02e981881e9
Alexis Couvreur <alexiscouvreur.pro@gmail.com>
2025-03-02 19:52:06 -0500

    Revert "test(testcontainers): use provider.PullImage"
    This reverts commit 6f958c48a559d3a95b98b787ce51b4d381d545f5.

--- pkg/provider/dockerswarm/testcontainers_test.go
+++ pkg/provider/dockerswarm/testcontainers_test.go
@@ -1,15 +1,18 @@
 package dockerswarm_test
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/image"
 	"github.com/docker/docker/api/types/swarm"
 	"github.com/docker/docker/client"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/wait"
 	"gotest.tools/v3/assert"
+	"slices"
 	"testing"
 )
 
@@ -53,6 +56,8 @@ func (d *dindContainer) CreateMimic(ctx context.Context, opts MimicOptions) (swa
 
 func setupDinD(t *testing.T, ctx context.Context) *dindContainer {
 	t.Helper()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	assert.NilError(t, err)
 
 	req := testcontainers.ContainerRequest{
 		Image:        "docker:dind",
@@ -69,7 +74,9 @@ func setupDinD(t *testing.T, ctx context.Context) *dindContainer {
 		Logger:           testcontainers.TestLogger(t),
 	})
 	assert.NilError(t, err)
-	testcontainers.CleanupContainer(t, c)
+	t.Cleanup(func() {
+		testcontainers.CleanupContainer(t, c)
+	})
 
 	ip, err := c.Host(ctx)
 	assert.NilError(t, err)
@@ -81,10 +88,7 @@ func setupDinD(t *testing.T, ctx context.Context) *dindContainer {
 	dindCli, err := client.NewClientWithOpts(client.WithHost(host), client.WithAPIVersionNegotiation())
 	assert.NilError(t, err)
 
-	provider, err := testcontainers.ProviderDocker.GetProvider()
-	assert.NilError(t, err)
-
-	err = provider.PullImage(ctx, "sablierapp/mimic:v0.3.1")
+	err = addMimicToDind(ctx, cli, dindCli)
 	assert.NilError(t, err)
 
 	// Initialize the swarm
@@ -98,4 +102,81 @@ func setupDinD(t *testing.T, ctx context.Context) *dindContainer {
 		client:    dindCli,
 		t:         t,
 	}
+}
+
+func searchMimicImage(ctx context.Context, cli *client.Client) (string, error) {
+	images, err := cli.ImageList(ctx, image.ListOptions{})
+	if err != nil {
+		return "", fmt.Errorf("failed to list images: %w", err)
+	}
+
+	for _, summary := range images {
+		if slices.Contains(summary.RepoTags, "sablierapp/mimic:v0.3.1") {
+			return summary.ID, nil
+		}
+	}
+
+	return "", nil
+}
+
+func pullMimicImage(ctx context.Context, cli *client.Client) error {
+	reader, err := cli.ImagePull(ctx, "sablierapp/mimic:v0.3.1", image.PullOptions{})
+	if err != nil {
+		return fmt.Errorf("failed to pull image: %w", err)
+	}
+	defer reader.Close()
+	resp, err := cli.ImageLoad(ctx, reader, true)
+	if err != nil {
+		return fmt.Errorf("failed to load image: %w", err)
+	}
+	defer resp.Body.Close()
+	return nil
+}
+
+func addMimicToDind(ctx context.Context, cli *client.Client, dindCli *client.Client) error {
+	ID, err := searchMimicImage(ctx, cli)
+	if err != nil {
+		return fmt.Errorf("failed to search for mimic image: %w", err)
+	}
+
+	if ID == "" {
+		err = pullMimicImage(ctx, cli)
+		if err != nil {
+			return err
+		}
+
+		ID, err = searchMimicImage(ctx, cli)
+		if err != nil {
+			return fmt.Errorf("failed to search for mimic image even though it's just been pulled without errors: %w", err)
+		}
+	}
+
+	reader, err := cli.ImageSave(ctx, []string{ID})
+	if err != nil {
+		return fmt.Errorf("failed to save image: %w", err)
+	}
+	defer reader.Close()
+
+	resp, err := dindCli.ImageLoad(ctx, reader, true)
+	if err != nil {
+		return fmt.Errorf("failed to load image in docker in docker container: %w", err)
+	}
+	defer resp.Body.Close()
+	buf := new(bytes.Buffer)
+	_, err = buf.ReadFrom(resp.Body)
+	if err != nil {
+		return fmt.Errorf("failed to read from response body: %w", err)
+	}
+
+	list, err := dindCli.ImageList(ctx, image.ListOptions{})
+	if err != nil {
+		return err
+	}
+
+	err = dindCli.ImageTag(ctx, list[0].ID, "sablierapp/mimic:v0.3.1")
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
