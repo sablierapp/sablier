dfec4995921a075ec205fd5e95748454fdcc44cb
Alexis Couvreur <alexiscouvreur.pro@gmail.com>
2025-03-03 22:26:19 -0500

    test(discovery): use providertest

--- app/discovery/autostop_test.go
+++ app/discovery/autostop_test.go
@@ -1,23 +1,25 @@
 package discovery_test
 
 import (
-	"context"
 	"errors"
 	"github.com/neilotoole/slogt"
 	"github.com/sablierapp/sablier/app/discovery"
 	"github.com/sablierapp/sablier/app/instance"
 	"github.com/sablierapp/sablier/app/types"
 	"github.com/sablierapp/sablier/pkg/provider"
-	"github.com/sablierapp/sablier/pkg/provider/mock"
+	"github.com/sablierapp/sablier/pkg/provider/providertest"
 	"github.com/sablierapp/sablier/pkg/store/inmemory"
+	gomock "go.uber.org/mock/gomock"
 	"gotest.tools/v3/assert"
 	"testing"
 	"time"
 )
 
 func TestStopAllUnregisteredInstances(t *testing.T) {
-	mockProvider := new(mock.ProviderMock)
-	ctx := context.TODO()
+	ctrl := gomock.NewController(t)
+	p := providertest.NewMockProvider(ctrl)
+
+	ctx := t.Context()
 
 	// Define instances and registered instances
 	instances := []types.Instance{
@@ -30,26 +32,25 @@ func TestStopAllUnregisteredInstances(t *testing.T) {
 	assert.NilError(t, err)
 
 	// Set up expectations for InstanceList
-	mockProvider.On("InstanceList", ctx, provider.InstanceListOptions{
+	p.EXPECT().InstanceList(ctx, provider.InstanceListOptions{
 		All:    false,
 		Labels: []string{discovery.LabelEnable},
 	}).Return(instances, nil)
 
 	// Set up expectations for InstanceStop
-	mockProvider.On("InstanceStop", ctx, "instance2").Return(nil)
-	mockProvider.On("InstanceStop", ctx, "instance3").Return(nil)
+	p.EXPECT().InstanceStop(ctx, "instance2").Return(nil)
+	p.EXPECT().InstanceStop(ctx, "instance3").Return(nil)
 
 	// Call the function under test
-	err = discovery.StopAllUnregisteredInstances(ctx, mockProvider, store, slogt.New(t))
+	err = discovery.StopAllUnregisteredInstances(ctx, p, store, slogt.New(t))
 	assert.NilError(t, err)
-
-	// Check expectations
-	mockProvider.AssertExpectations(t)
 }
 
 func TestStopAllUnregisteredInstances_WithError(t *testing.T) {
-	mockProvider := new(mock.ProviderMock)
-	ctx := context.TODO()
+	ctrl := gomock.NewController(t)
+	p := providertest.NewMockProvider(ctrl)
+
+	ctx := t.Context()
 
 	// Define instances and registered instances
 	instances := []types.Instance{
@@ -62,19 +63,16 @@ func TestStopAllUnregisteredInstances_WithError(t *testing.T) {
 	assert.NilError(t, err)
 
 	// Set up expectations for InstanceList
-	mockProvider.On("InstanceList", ctx, provider.InstanceListOptions{
+	p.EXPECT().InstanceList(ctx, provider.InstanceListOptions{
 		All:    false,
 		Labels: []string{discovery.LabelEnable},
 	}).Return(instances, nil)
 
 	// Set up expectations for InstanceStop with error
-	mockProvider.On("InstanceStop", ctx, "instance2").Return(errors.New("stop error"))
-	mockProvider.On("InstanceStop", ctx, "instance3").Return(nil)
+	p.EXPECT().InstanceStop(ctx, "instance2").Return(errors.New("stop error"))
+	p.EXPECT().InstanceStop(ctx, "instance3").Return(nil)
 
 	// Call the function under test
-	err = discovery.StopAllUnregisteredInstances(ctx, mockProvider, store, slogt.New(t))
+	err = discovery.StopAllUnregisteredInstances(ctx, p, store, slogt.New(t))
 	assert.Error(t, err, "stop error")
-
-	// Check expectations
-	mockProvider.AssertExpectations(t)
 }
---
+++ pkg/provider/kubernetes/instance_inspect_test.go
@@ -0,0 +1,48 @@
+package kubernetes_test
+
+import (
+	"context"
+	"fmt"
+	"github.com/neilotoole/slogt"
+	"github.com/sablierapp/sablier/config"
+	"github.com/sablierapp/sablier/pkg/provider/kubernetes"
+	"gotest.tools/v3/assert"
+	"testing"
+)
+
+func TestKubernetesProvider_InstanceInspect(t *testing.T) {
+	ctx := context.Background()
+	type args struct {
+		name string
+	}
+	tests := []struct {
+		name string
+		args args
+		want error
+	}{
+		{
+			name: "invalid format name",
+			args: args{
+				name: "invalid-name-format",
+			},
+			want: fmt.Errorf("invalid name [invalid-name-format] should be: kind_namespace_name_replicas"),
+		},
+		{
+			name: "unsupported resource name",
+			args: args{
+				name: "service_default_my-service_1",
+			},
+			want: fmt.Errorf("unsupported kind \"service\" must be one of \"deployment\", \"statefulset\""),
+		},
+	}
+	c := setupKinD(t, ctx)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			p, err := kubernetes.NewKubernetesProvider(ctx, c.client, slogt.New(t), config.NewProviderConfig().Kubernetes)
+
+			_, err = p.InstanceInspect(ctx, tt.args.name)
+			assert.Error(t, err, tt.want.Error())
+		})
+	}
+}
