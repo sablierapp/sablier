781deac77f5dc3ec8c665b95787940d3b7d6e84e
Alexis Couvreur <alexiscouvreur.pro@gmail.com>
2026-05-31 18:04:44 -0400

    rm unrelated files

--- internal/openapi/embed.go
+++
@@ -1,9 +0,0 @@
-package openapi
-
-import _ "embed"
-
-// Spec is the OpenAPI 3.1 specification for the Sablier HTTP API,
-// embedded at build time from the YAML source file.
-//
-//go:embed openapi.yaml
-var Spec []byte
